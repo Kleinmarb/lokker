ce3d3593bc80659ccea6f3579cb560e8978fbe33
Kleinmarb <selimkentel@gmail.com>
2024-01-18 16:46:56 +0100

    remove cached files

--- target/debug/deps/lokker-38871927ad809f28.d
+++
@@ -1,8 +0,0 @@
-/home/asuna/rust-workspace/lokker/target/debug/deps/liblokker-38871927ad809f28.rmeta: src/lib.rs src/time.rs
-
-/home/asuna/rust-workspace/lokker/target/debug/deps/liblokker-38871927ad809f28.rlib: src/lib.rs src/time.rs
-
-/home/asuna/rust-workspace/lokker/target/debug/deps/lokker-38871927ad809f28.d: src/lib.rs src/time.rs
-
-src/lib.rs:
-src/time.rs:
--- target/debug/liblokker.d
+++
@@ -1,1 +0,0 @@
-/home/asuna/rust-workspace/lokker/target/debug/liblokker.rlib: /home/asuna/rust-workspace/lokker/src/lib.rs /home/asuna/rust-workspace/lokker/src/time.rs
